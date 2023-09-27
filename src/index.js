7764ad0b2ea837e3662405f823af0eb2ba85f37e
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-09-27 15:36:18 -0400

    created footer component

--- src/index.js
+++ src/index.js
@@ -50,9 +50,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Menu />
       <Footer />
     </div>
   );
@@ -62,10 +60,30 @@ function Header() {
   return <h1>Fast React Pizza Co.</h1>;
 }
 
-function Menu() {}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
 
 function Footer() {
-  return React.createElement("footer", null, "We're currently open!");
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+//   if (hour >= openHour && hour <= closeHour) alert("We're currently open");
+//   else alert("Sorry we are closed!");
+
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+  );
 }
 
 function Pizza() {
