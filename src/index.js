36a5d598b3da759ccc0fde769ddadc650ccf5a6c
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-01 09:43:27 -0400

    finished pizza project

--- src/index.js
+++ src/index.js
@@ -95,27 +95,16 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut)
-    return (
-      <li className="pizza sold-out">
-        <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-        <div>
-          <h3>{pizzaObj.name}</h3>
-          <p>{pizzaObj.ingredients}</p>
-          <span>{pizzaObj.price}</span>
-        </div>
-      </li>
-    );
 
   return (
-    <li className="pizza">
+    <li className={pizzaObj.soldOut ? "pizza sold-out" : "pizza"}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
-    </li>
+    </li> 
   );
 }
 
